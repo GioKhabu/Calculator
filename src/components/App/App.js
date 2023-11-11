8a76f02b332ecaf1f727304e783230e908dbf2f1
unknown <giokhabu@gamil.com>
2023-11-11 15:22:11 +0400

    added theme context and them change effect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,37 +1,47 @@
 import React from 'react';
 
-import styles from  './App.module.css';
-import '../Calculator'
+import styles from './App.module.css';
+import '../Calculator';
 import Calculator from '../Calculator';
 import { Theme1, Theme2, Theme3 } from '../../utils';
 
+export const ThemeColorContext = React.createContext();
+
 function App() {
-  const[theme, setTheme] = React.useState(Theme1)
+  const [theme, setTheme] = React.useState(Theme1);
+
+  const value = {
+    theme,
+    setTheme
+  }
+
   return (
-    <div
-      className={`${styles.App}`}
-      style={{
-        '--color-mainBackground': theme.mainBackground,
-        '--color-screenBackground': theme.screenBackground,
-        '--color-keypadBackground': theme.keypadBackground,
+    <ThemeColorContext.Provider value={value}>
+      <div
+        className={`${styles.App}`}
+        style={{
+          '--color-mainBackground': theme.mainBackground,
+          '--color-screenBackground': theme.screenBackground,
+          '--color-keypadBackground': theme.keypadBackground,
 
-        '--color-keyBackground': theme.keyBackground,
-        '--color-keyShadow': theme.keyShadow,
+          '--color-keyBackground': theme.keyBackground,
+          '--color-keyShadow': theme.keyShadow,
 
-        '--color-equalKeyBackground': theme.equalKeyBackground,
-        '--color-equalKeyBackgroundShadow': theme.equalKeyBackgroundShadow,
+          '--color-equalKeyBackground': theme.equalKeyBackground,
+          '--color-equalKeyBackgroundShadow': theme.equalKeyBackgroundShadow,
 
-        '--color-secondaryKeyBackground': theme.secondaryKeyBackground,
-        '--color-secondaryKeyBackShadow': theme.secondaryKeyBackShadow,
+          '--color-secondaryKeyBackground': theme.secondaryKeyBackground,
+          '--color-secondaryKeyBackShadow': theme.secondaryKeyBackShadow,
 
-        '--color-headPanelText': theme.headPanelText,
-        '--color-keyBoardText': theme.keyBoardText,
-        '--color-primaryText': theme.primaryText,
-        '--color-equalText': theme.equalText,
-      }}
-    >
-      <Calculator />
-    </div>
+          '--color-headPanelText': theme.headPanelText,
+          '--color-keyBoardText': theme.keyBoardText,
+          '--color-primaryText': theme.primaryText,
+          '--color-equalText': theme.equalText,
+        }}
+      >
+        <Calculator />
+      </div>
+    </ThemeColorContext.Provider>
   );
 }
 
--- src/components/ThemeControler/ThemeControler.js
+++ src/components/ThemeControler/ThemeControler.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import styles from './ThemeControler.module.css'
+import { Theme1, Theme2, Theme3 } from '../../utils';
+
+
+import { ThemeColorContext } from '../App/App';
 
 function ThemeControler() {
+const { theme, setTheme } = React.useContext(ThemeColorContext);
+const [volume, setVolume] = React.useState(1)
 
+React.useEffect(()=>{
+  const volumeN = Number(volume);
+
+ if (volumeN === 1) {
+   setTheme(Theme1);
+ } else if (volumeN === 2) {
+   setTheme(Theme2);
+ } else if (volumeN === 3) {
+   setTheme(Theme3);
+ }
+}, [volume, setTheme])
   return (
     <div className={`${styles.themeGroup}`}>
-      <form className={`${styles.themeWrapper}`}>
+      <form className={`${styles.themeWrapper}`} onSubmit={event => {
+        event.preventDefault()
+      }}>
         <label htmlFor="theme-slider" className={`${styles.themeLabel}`}>
           THEME
         </label>
@@ -21,6 +40,10 @@ function ThemeControler() {
             min={1}
             step={1}
             max={3}
+            value={volume}
+            onChange={event => {
+              setVolume(event.target.value)
+            }}
             className={`${styles.themeRange}`}
           />
         </div>
